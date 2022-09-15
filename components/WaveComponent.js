3225ea64523928ffe12c95a227d05c23795fc5ce
cotlowzky <cotlowzky@gmail.com>
2022-09-15 11:02:45 +0200

    add wave to page

---
+++ components/WaveComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WaveComponent() {
+  return (
+    <div className='wave flex'></div>
+  )
+}
+
+export default WaveComponent
--- sections/HeroSection.js
+++ sections/HeroSection.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import WaveComponent from '../components/WaveComponent'
 import styles from '../styles/hero.module.css'
+
 function HeroSection() {
   return (
     <div className={`flex flex-column flex-center ${styles.heroWrapper}`}>
         <h1 className='montserrat-bold text-32 text-center'>Hi, my name is <span className='text-yellow'>Filip Kotłowski</span></h1>
         <h3 className={`montserrat-bold text-112 text-upper ${styles.heroText}`}>web developer</h3>
-        <h5 className='incon-bold text-20'>based in Opole, Poland</h5>
+        <h5 className='incon-bold text-20 flex-left'>based in Opole, Poland</h5>
+        <img className='heroImage' src="/_next/static/media/person-stock.a9bdd91e.png" alt="smiling guy"/>
+        <WaveComponent />
     </div>
   )
 }
