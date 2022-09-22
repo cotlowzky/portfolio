54695aea4d1eb8f21810a77dc7ee5411dc6162cb
cotlowzky <cotlowzky@gmail.com>
2022-09-22 10:25:15 +0200

    work section v1.02

--- sections/WorkSection.js
+++ sections/WorkSection.js
@@ -2,6 +2,7 @@ import React from 'react'
 function WorkSection(props) {
   return (
     <div className="flex">
+    <h2 className='montserrat-bold text-black text-96 text-title'>Works</h2>
       {props.content}
     </div>
   )
