d37043542cdb4bf72db1c504dcad1390cebda0e8
cotlowzky <cotlowzky@gmail.com>
2022-09-28 09:16:46 +0200

    end of work section

--- sections/WorkSection.js
+++ sections/WorkSection.js
@@ -1,9 +1,11 @@
 import React from 'react'
 function WorkSection(props) {
   return (
-    <div className="flex">
-    <h2 className='montserrat-bold text-black text-96 text-title'>Works</h2>
-      {props.content}
+    <div className="flex flex-column">
+    <h2 className='montserrat-bold text-black text-96 text-title text-title-work'>Works</h2>
+      <div className='flex'>
+        {props.content}
+      </div>
     </div>
   )
 }
