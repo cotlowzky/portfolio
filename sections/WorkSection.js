30421c43b5ce50f54dfd6f3ca3d70a95c951532a
cotlowzky <cotlowzky@gmail.com>
2022-10-03 10:54:13 +0200

    smooth scrolling and better design

--- sections/WorkSection.js
+++ sections/WorkSection.js
@@ -1,7 +1,7 @@
 import React from 'react'
 function WorkSection(props) {
   return (
-    <div className="flex flex-column" id="work">
+    <div className="flex flex-column p-5top" id="work">
     <h2 className='montserrat-bold text-black text-96 text-title text-title-work'>Works</h2>
       <div className='flex'>
         {props.content}
