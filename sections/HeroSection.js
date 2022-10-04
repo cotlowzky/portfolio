504a7785a193e566cb797eec156fd7c6f42d4765
cotlowzky <cotlowzky@gmail.com>
2022-10-04 13:09:57 +0200

    mobile rensponsive

--- sections/HeroSection.js
+++ sections/HeroSection.js
@@ -6,9 +6,9 @@ import Image from 'next/image'
 function HeroSection() {
   return (
     <div className={`flex flex-column flex-center ${styles.heroWrapper}`}>
-        <h1 className='montserrat-bold text-32 text-center'>Hi, my name is <span className='text-yellow'>Filip Kotłowski</span></h1>
-        <h3 className={`montserrat-bold text-112 text-upper ${styles.heroText}`}>web developer</h3>
-        <h5 className='incon-bold text-20 flex-left'>based in Opole, Poland</h5>
+        <h1 className='montserrat-bold text-32 text-center name'>Hi, my name is <span className='text-yellow'>Filip Kotłowski</span></h1>
+        <h3 className={`montserrat-bold text-112 text-upper work-text ${styles.heroText}`}>web developer</h3>
+        <h5 className='incon-bold text-20 flex-left location'>based in Opole, Poland</h5>
         <div className='heroImage'>
           <Image src={heroImage} alt="smiling guy" height="650px" width="600px"/>
         </div>
--- sections/WorkSection.js
+++ sections/WorkSection.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from '../styles/work.module.css'
+
 function WorkSection(props) {
   return (
     <div className="flex flex-column p-5top" id="work">
     <h2 className='montserrat-bold text-black text-96 text-title text-title-work'>Works</h2>
-      <div className='flex'>
+      <div className={`flex ${styles.websites}`}>
         {props.content}
       </div>
     </div>
