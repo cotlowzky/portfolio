ccd2adee911eef1cf993399ed4bde44ea2a0a0e0
cotlowzky <cotlowzky@gmail.com>
2022-10-05 12:35:05 +0200

    change contact buttons

--- sections/ContactSection.js
+++ sections/ContactSection.js
@@ -7,14 +7,14 @@ function ContactSection() {
     <div className='flex w-100p flex-column m-5btm p-5top' id="contact"> 
         <h2 className='montserrat-bold text-black text-96 text-title contact m-5btm'>Contact</h2>
         <div className='flex contact-section'>
-            <div className='w-50p h-20 contact-photo'>
+            <div className='w-50p contact-photo'>
                 <Image src={contactImage}/>
             </div>
-            <div className='w-50p h-20 flex-space-btwn flex-column flex contact-buttons'>
-                <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton} ${styles.github} `}><p>GitHub</p></div>
-                <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton} ${styles.linkedin}`}><p>LinkedIn</p></div>
-                <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton} ${styles.mail}`}><p>Mail</p></div>
-                <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton} ${styles.phone}`}><p>Phone</p></div>
+            <div className='w-50p flex-space-around flex-column flex contact-buttons m-3'>
+                <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton} `}><p>GitHub</p></div>
+                <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton} `}><p>LinkedIn</p></div>
+                <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton}`}><p>Mail</p></div>
+                <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton}`}><p>Phone</p></div>
             </div>
         </div>
     </div>
