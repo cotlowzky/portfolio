a6e6ecd0416bc62e60d96def04db77dc90972e84
cotlowzky <cotlowzky@gmail.com>
2022-10-05 12:29:48 +0200

    change responsive

--- sections/ContactSection.js
+++ sections/ContactSection.js
@@ -6,11 +6,11 @@ function ContactSection() {
   return (
     <div className='flex w-100p flex-column m-5btm p-5top' id="contact"> 
         <h2 className='montserrat-bold text-black text-96 text-title contact m-5btm'>Contact</h2>
-        <div className='flex'>
-            <div className='w-50p h-20'>
+        <div className='flex contact-section'>
+            <div className='w-50p h-20 contact-photo'>
                 <Image src={contactImage}/>
             </div>
-            <div className='w-50p h-20 flex-space-btwn flex-column flex'>
+            <div className='w-50p h-20 flex-space-btwn flex-column flex contact-buttons'>
                 <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton} ${styles.github} `}><p>GitHub</p></div>
                 <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton} ${styles.linkedin}`}><p>LinkedIn</p></div>
                 <div className={`montserrat-bold  text-20 text-white text-small-caps p-4 m-1 ${styles.contactButton} ${styles.mail}`}><p>Mail</p></div>
